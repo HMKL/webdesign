36f22e059dc66a79d5a8902e48c924e76dabbc5f
Catalin B <catalin.bac@gmail.com>
2022-11-21 09:42:19 +0200

    Add files via upload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,18 +10,52 @@
     getBtn.classList.toggle('active');
       getMenu.classList.toggle('active');
   });
+
+
+
+
+
+
+  var new_scroll_position = 0;
+  var last_scroll_position;
+  var header = document.querySelector('.top.bar');
+  
+  window.addEventListener('scroll', function(e) {
+    last_scroll_position = window.scrollY;
+  
+    // Scrolling down
+    if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+      // header.removeClass('slideDown').addClass('slideUp');
+      header.classList.remove("slideDown");
+      header.classList.add("slideUp");
+  
+    // Scrolling up
+    } else if (new_scroll_position > last_scroll_position) {
+      // header.removeClass('slideUp').addClass('slideDown');
+      header.classList.remove("slideUp");
+      header.classList.add("slideDown");
+    }
+  
+    new_scroll_position = last_scroll_position;
+  });
+
+
+
+console.log(header);
+
+
 })();
 
-const getNav = document.querySelector('.top.bar');
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    getNav.style.top = "0";
+// const getNav = document.querySelector('.top.bar');
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     getNav.style.top = "0px";
    
-  } else {
-    getNav.style.top = "-64px";
+//   } else {
+//     getNav.style.top = "-64px";
   
-  }
-  prevScrollpos = currentScrollPos;
-}
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
