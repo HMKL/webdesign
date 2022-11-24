2cc3c04ef5a8ce5532fe2ece1e4ebb094c1c69ba
Catalin B <catalin.bac@gmail.com>
2022-11-24 14:21:53 +0200

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -33,14 +33,14 @@ let layoutHandler;
   'use strict';
   // Your custom JavaScript goes here
 
-
+ const getBody = document.querySelector('body');
   const getBtn = document.querySelector('.hamburger-menu');
   const getMenu = document.querySelector('.links');
   getBtn.addEventListener('click', () =>{
     getBtn.classList.toggle('active');
       getMenu.classList.toggle('active');
+      getBody.classList.toggle('over');
   });
-
 
 
 
@@ -55,11 +55,13 @@ let layoutHandler;
     if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
       header.classList.remove("slideDown");
       header.classList.add("slideUp");
+      // header.classList.remove('absolute');
+      // header.classList.add('fixed');
   
     } else if (new_scroll_position > last_scroll_position) {
       header.classList.remove("slideUp");
       header.classList.add("slideDown");
-    }
+    } 
   
     new_scroll_position = last_scroll_position;
   });
@@ -79,6 +81,9 @@ function value() {
   var scroll = window.scrollY;
   if (scroll < 200) {
       navbar.classList.remove('color');
+      // navbar.classList.remove('fixed');
+      // navbar.classList.add('absolute');
+
   } else {
       navbar.classList.add('color');
   }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,14 @@
   'use strict';
   // Your custom JavaScript goes here
 
-
+ const getBody = document.querySelector('body');
   const getBtn = document.querySelector('.hamburger-menu');
   const getMenu = document.querySelector('.links');
   getBtn.addEventListener('click', () =>{
     getBtn.classList.toggle('active');
       getMenu.classList.toggle('active');
+      getBody.classList.toggle('over');
   });
-
 
 
 
@@ -25,11 +25,13 @@
     if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
       header.classList.remove("slideDown");
       header.classList.add("slideUp");
+      // header.classList.remove('absolute');
+      // header.classList.add('fixed');
   
     } else if (new_scroll_position > last_scroll_position) {
       header.classList.remove("slideUp");
       header.classList.add("slideDown");
-    }
+    } 
   
     new_scroll_position = last_scroll_position;
   });
@@ -49,6 +51,9 @@ function value() {
   var scroll = window.scrollY;
   if (scroll < 200) {
       navbar.classList.remove('color');
+      // navbar.classList.remove('fixed');
+      // navbar.classList.add('absolute');
+
   } else {
       navbar.classList.add('color');
   }
